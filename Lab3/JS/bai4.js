418987b6ddb571a18a05a2ad1293ed430c8179d4
canh1703 <132972179+canh1703@users.noreply.github.com>
2024-01-20 22:05:38 +0700

    Delete Lab3 directory

--- Lab3/JS/bai4.js
+++
@@ -1,9 +0,0 @@
-function spreadOut() {
-    let fragment = ['to', 'code'];
-    let sentence = ['learning', ...fragment, 'is', 'fun'];
-    return sentence;
-}
-// console.log(spreadOut());
-const result = spreadOut();
-const resultDiv = document.getElementById('result');
-resultDiv.innerHTML = `<ul>${result.map(item => `<li>${item}</li>`).join('')}</ul>`;
